83f1fb3fb0cfc9def7259d04e2333d9bdba50d3f
draxBlob27 <sp27022003@sanil-cpp-2026.us-central1-a.c.gen-lang-client-0704258934.internal>
2026-02-02 16:16:21 +0000

    Error handler using std::runtime implemented

---
+++ Binary_Vector_Archive_with_Integrity_Checks/include/vectorDB/errors.h
@@ -0,0 +1,35 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#include <stdexcept>
+#include <string>
+
+class ArchiveError : public std::runtime_error {
+public:
+    ArchiveError(const std::string& error) 
+        :std::runtime_error{error}
+    {}
+};
+
+class FileNotFoundError : public ArchiveError {
+public:
+    FileNotFoundError(const std::string& error) 
+        :ArchiveError(error)
+    {}
+};
+
+class CorruptedDataError : public ArchiveError {
+public:
+    CorruptedDataError(const std::string& error)
+        :ArchiveError(error)
+    {}
+};
+
+class InsufficientSpaceError : public ArchiveError {
+public:
+    InsufficientSpaceError(const std::string& error)
+        :ArchiveError(error)
+    {}
+};
+
+#endif
