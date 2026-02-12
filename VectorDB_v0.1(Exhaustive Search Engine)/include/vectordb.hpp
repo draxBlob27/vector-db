c15cb754f091ceb70da577fc966cd37c50ca9071
sanil <sparmar27feb2003@gmail.com>
2026-02-13 01:22:39 +0530

    feat: Added DBError enum class, completed query, finalized struct Vector, also add error handling.

--- VectorDB_v0.1(Exhaustive Search Engine)/include/vectordb.hpp
+++ VectorDB_v0.1(Exhaustive Search Engine)/include/vectordb.hpp
@@ -3,23 +3,22 @@
 #include <stdexcept>
 #include <algorithm>
 #include <cmath>
+#include <queue>
+#include <functional>
+#include <unordered_map>
 #include <variant>
 #include <vector>
 
-struct Vector {
-    std::vector<float> data;
-    bool normalized;
-    float norm() const {
-        ;
-    }
-    void normalize();
-};
+//TODO -> handle diff k values.
 
 enum class Metric {
-    L2, 
-    Cosine, 
-    DotProduct
-};
+    L2, //smaller is better
+    Cosine, //larger is better
+    DotProduct //larger is better
+}; 
+
+// typedef int32_t status_t;
+// #define S_BAD_METRIC (-3)
 
 template <typename T>
 class Ok {
@@ -39,8 +38,6 @@ public:
         return std::move(value);
     }
 };
-
-
 
 template <typename T>
 class Err {
@@ -94,7 +91,7 @@ class Result {
     }
 };
 
-struct Unit{};
+struct Unit{}; //replaces void return type with empty class type, inspried from article linked.
 template<typename ErrT>
 class Result<Unit, ErrT> {
     std::variant<Ok<Unit>, Err<ErrT>> variant;
@@ -130,36 +127,190 @@ class Result<Unit, ErrT> {
     }
 };
 
+enum class DBError : std::int32_t {
+    MetricError = (-1),
+    DimensionError = (-2),
+    IdNotFoundError = (-3),
+    ZeroNormError = (-4),
+    DataBaseEmptyError = (-5)
+};
+
+struct Vector {
+    std::vector<float> data;
+    float norm_data{0.0f};
+    bool normalized{false};
+
+    float norm() const {
+        return norm_data;
+    }
+
+    void computed_norm() {
+        for (const auto& it : data) {
+            norm_data += (it * it);
+        }
+
+        norm_data = std::sqrt(norm_data);
+        normalized = true;
+    };
+};
 
 class VectorStore {
 private:
     std::vector<std::pair<uint64_t, Vector>> m_vectors;
 
 public:
-    Result<Unit, std::runtime_error> insert(std::uint64_t id, const Vector& vector) {
+    Result<Unit, DBError> insert(std::uint64_t id, Vector& i_vector) { //need to handle Id already exists
         auto dims_valid{[&]() {
-            return m_vectors[0].second.data.size() == vector.data.size();
+            return m_vectors[0].second.data.size() == i_vector.data.size();
         }};
 
         if (m_vectors.empty() || dims_valid()) {
-            m_vectors.push_back({id, vector});
-            return;
+            i_vector.computed_norm(); //normalise at insgestion
+
+            m_vectors.push_back({id, std::move(i_vector)}); //used move because it could be(i think..) wil see later on).
+            return Ok<Unit>(Unit{});
         }
 
-        throw std::runtime_error("Dimensions mismatch.");
+        return Err<DBError>(DBError::DimensionError);
     }
 
-    Result<Unit, std::runtime_error> remove(std::uint64_t id) {
+    Result<Unit, DBError> remove(std::uint64_t id) {
         auto find_id{[&id](std::pair<std::uint64_t, Vector>& a) {
             return a.first == id;
         }};
         auto new_logical_end = std::remove_if(m_vectors.begin(), m_vectors.end(), find_id);
         // auto new_logical_end = std::ranges::remove_if(m_vectors, find_id);
+
+        if (new_logical_end == m_vectors.end()) { //incase of id not found
+            return Err<DBError>(DBError::IdNotFoundError);
+        }
+
         m_vectors.erase(new_logical_end, m_vectors.end());
+        return Ok<Unit>(Unit{});
     }   
 
-    Result<std::vector<std::pair<std::uint64_t, float>>, std::runtime_error> query(const std::vector<float>& vector, int k = 10, Metric metric){
-        
+    Result<std::vector<std::pair<std::uint64_t, float>>, DBError> query(Vector& q_vector, int k = 10, Metric metric) {//very large object is getting created, can think of move semantics
+        //very high chances of using quick select, just saw LC soln today(12 feb, 2026) regarding this
+        //saying quick select is best in terms of TC ~ O(n) for best k kinda things
+        if (m_vectors.empty()) {
+            return Err<DBError>{DBError::DataBaseEmptyError};
+        }
+
+        std::vector<std::pair<std::uint64_t, float>> res(std::min(static_cast<uint64_t>(k), size())); //handles if DB size is less than k.
+        int i{res.size() - 1};
+
+        switch (metric)
+        {
+        case(Metric::L2): {
+            std::priority_queue<std::pair<float, std::uint64_t>> pq;
+            
+            if (m_vectors[0].second.data.size() != q_vector.data.size()) { //no need for each data point check because these are already verified at insertion.
+                return Err<DBError>{DBError::DimensionError};
+            }
+
+            for (const auto& it : m_vectors) {
+                float distance{0.0f};
+                /* code */
+                //it.first -> id
+                //it.second -> Vector
+                
+                for (int i{0}; i < it.second.data.size(); i++) {
+                    distance += (it.second.data[i] - q_vector.data[i]) * (it.second.data[i] - q_vector.data[i]);
+                }
+
+                if (pq.size() < res.size()) { //smaller is better
+                    pq.push({distance, it.first}); //holds the id
+                } else if (pq.top().first > distance) {
+                    pq.pop();
+                    pq.push({distance, it.first});
+                }
+            }
+
+            while (!pq.empty()) {
+                res[i--] = {pq.top().second, pq.top().first};
+                pq.pop();
+            }
+            break;
+        }
+        case(Metric::DotProduct): {
+            std::priority_queue<std::pair<float, std::uint64_t>, std::vector<std::pair<float, std::uint64_t>>, std::greater<>> pq;
+            
+            if (m_vectors[0].second.data.size() != q_vector.data.size()) { //no need for each data point check because these are already verified at insertion.
+                return Err<DBError>{DBError::DimensionError};
+            }
+
+            for (const auto& it : m_vectors) {
+                float distance{0.0f};
+                /* code */
+                //it.first -> id
+                //it.second -> Vector
+
+                for (int i{0}; i < it.second.data.size(); i++) {
+                    distance += (it.second.data[i] * q_vector.data[i]);
+                }
+
+                if (pq.size() < res.size()) { //larger is better due to similarity -> vector more aligned
+                    pq.push({distance, it.first}); //holds the id
+                } else if (pq.top().first < distance) {
+                    pq.pop();
+                    pq.push({distance, it.first});
+                }
+            }
+
+            while (!pq.empty()) {
+                res[i--] = {pq.top().second, pq.top().first};
+                pq.pop();
+            }
+            break;
+        }
+        case (Metric::Cosine): {
+            std::priority_queue<std::pair<float, std::uint64_t>, std::vector<std::pair<float, std::uint64_t>>, std::greater<>> pq;
+            
+            q_vector.computed_norm(); //perform normailzation of query vector
+            float query_norm{q_vector.norm()};
+            //if query norm is 0 then outright riject it.
+            if (query_norm == 0.0f) {
+                return Err<DBError>{DBError::ZeroNormError};
+            }
+
+            if (m_vectors[0].second.data.size() != q_vector.data.size()) { //no need for each data point check because these are already verified at insertion.
+                return Err<DBError>{DBError::DimensionError};
+            }
+            
+            for (const auto& it : m_vectors) {
+                float distance{0.0f};
+                /* code */
+                //it.first -> id
+                //it.second -> Vector
+                if (it.second.norm() == 0.0f) {
+                    continue;
+                }
+
+                for (int i{0}; i < it.second.data.size(); i++) {
+                    distance += (it.second.data[i] * q_vector.data[i]);
+                }
+
+                distance /= (it.second.norm() * query_norm);
+
+                if (pq.size() < res.size()) {
+                    pq.push({distance, it.first}); //holds the id
+                } else if (pq.top().first < distance) {
+                    pq.pop();
+                    pq.push({distance, it.first});
+                }
+            }
+
+            while (!pq.empty()) {
+                res[i--] = {pq.top().second, pq.top().first};
+                pq.pop();
+            }
+            break;
+        }
+        default:
+            return Err<DBError>{DBError::MetricError};
+        }
+
+        return Ok(res);
     }
 
     void save(const std::string& filename) {
@@ -171,44 +322,12 @@ public:
     }
 
     std::uint64_t size() {
-
+        return m_vectors.size();
     }
 
     std::uint64_t dimensions() {
-
+        return m_vectors[0].second.data.size();
     }
 
 };
 
-template <Metric M> //can use auto as well
-float distance(const Vector& a, const Vector& b);
-
-template<> float distance<Metric::L2>(const Vector& a, const Vector& b) {
-    float res = 0;
-
-    for (int i{0}; i < a.data.size(); i++) {
-        res += (a.data[i] - b.data[i]) * (a.data[i] - b.data[i]);
-    }
-
-    return std::sqrt(res);
-}
-
-template<> float distance<Metric::Cosine>(const Vector& a, const Vector& b) {
-    if (a.normalized && b.normalized) {
-        return distance<Metric::DotProduct>(a, b);
-    }
-
-    float res = distance<Metric::DotProduct>(a, b);
-
-    return res / (a.norm() * b.norm());
-}
-
-template<> float distance<Metric::DotProduct>(const Vector& a, const Vector& b) {
-    float res = 0;
-
-    for (int i{0}; i < a.data.size(); i++) {
-        res += (a.data[i] - b.data[i]);
-    }
-
-    return res;
-}
--- VectorDB_v0.1(Exhaustive Search Engine)/learnings.md
+++ VectorDB_v0.1(Exhaustive Search Engine)/learnings.md
@@ -1,7 +1,14 @@
 1. [Erase Remove idiom](https://www.geeksforgeeks.org/cpp/erase-remove-idiom-in-cpp/)
 
-2. std::variant handling was confusing, required help from articles + AI. [Modern C++: Error Handling with Result](https://yegor.pomortsev.com/post/result-type/), [C++ void template parameter](https://lifecs.likai.org/2010/04/c-void-template-parameter.html)
+2. std::variant handling was confusing, required help from articles + AI. [Modern C++: Error Handling with Result Type](https://yegor.pomortsev.com/post/result-type/), [C++ void template parameter](https://lifecs.likai.org/2010/04/c-void-template-parameter.html)
 
 3. To handle result<void, E> -> need to write template specialization. 
 
-4. Read 26 learnCpp tomorrow
+4. I was mixing, runtime polymorphism and compile time polymorhism.
+I declared distance functions as templated -> template paramter Metric must be provided at compile time
+My API or method in VectorStore was expecting metric from user -> runtime variable
+Hence that was a design issue.
+So for runtime dispatch i would use switch statements.
+[when-is-virtual-dispatch-faster-than-function-templates-in-c-runtime](https://stackoverflow.com/questions/79219324/when-is-virtual-dispatch-faster-than-function-templates-in-c-runtime)
+
+
